6ba105804819f1574266fb789e49fccc3d312f71
RakhshanKhalid <rakhshan0602@gmail.com>
2021-02-19 17:12:15 +0530

    Update index.py
    Second Commit

--- index.py
+++ index.py
@@ -2,40 +2,78 @@ from urllib.request import urlopen,Request
 from bs4 import BeautifulSoup as bs
 import re 
 import json
+import flask
+from flask import jsonify
+
+#flask
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
 #website url
-base_url = "https://123animes.mobi/filter?country%5B%5D=j&status%5B%5D=ongoing&keyword="
-url_="https://123animes.mobi"
-url = base_url
+base_url = "https://123animes.mobi"
 
-#Requesting the html source code
-url_link = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-url_page = urlopen(url_link).read()
-url_html = bs(url_page, 'html.parser')
 
-containers = url_html.find_all('div',class_='film-list')
 
-#looping through containers to extract more detailed info
-for items in containers[:1]:
-    animes_url_name = items.find_all('a',class_ = "name") #extracting name/title of all Anime in the current page
-    anime_image = items.find_all('img')
-    anime_lang = items.find_all(True, {"class":["sub", "dub"]})
 
-#anime url and name
-array_length = len(animes_url_name)
-anime_data=[0]*array_length
 
-#store collected data in list
-data=[]
-for x in range(array_length):
-    item={}
-    item['title'] = animes_url_name[x].get('data-jtitle')
-    item['url'] = url_+animes_url_name[x].get('href')
-    item['lang'] = anime_lang[x].get_text()
-    item['image'] = anime_image[x].get('src')
-    data.append(item)
+def getData(url): 
+    #Requesting the html source code
+    url_link = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    url_page = urlopen(url_link).read()
+    url_html = bs(url_page, 'html.parser')
+
+    containers = url_html.find_all('div',class_='film-list')
+
+    #looping through containers to extract more detailed info
+    for items in containers[:1]:
+        animes_url_name = items.find_all('a',class_ = "name") #extracting name/title and URL of all Anime in the current page
+        anime_image = items.find_all('img') #extracting IMAGE all Anime in the current page
+        anime_lang = items.find_all(True, {"class":["sub", "dub"]}) #extracting LANGUAGE of all Anime in the current page
+
+    #anime url and name
+    array_length = len(animes_url_name)
+
+    #store collected data in list
+    data=[]
+    for x in range(array_length):
+        item={}
+        item['title'] = animes_url_name[x].get('data-jtitle')
+        item['url'] = base_url+animes_url_name[x].get('href')
+        item['lang'] = anime_lang[x].get_text()
+        item['image'] = anime_image[x].get('src')
+        data.append(item)
     
-#convertind list to json
-with open("textbooks.json", "w") as writeJSON:
-    json.dump(data, writeJSON, ensure_ascii=False)
+    return data
 
+
+
+
+@app.route('/home', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+@app.route('/api/v1/airing', methods=['GET'])
+def airin_anime():
+    url_ = base_url + "/filter?status[]=ongoing&keyword="
+    data_ = getData(url_)
+    return jsonify(data_)
+
+@app.route('/api/v1/upcoming', methods=['GET'])
+def upcoming_anime():
+    url_ = base_url + "/filter?status[]=upcoming&keyword="
+    data_ = getData(url_)
+    return jsonify(data_)
+
+@app.route('/api/v1/finished', methods=['GET'])
+def finished_anime():
+    url_ = base_url + "/filter?status[]=completed&keyword="
+    data_ = getData(url_)
+    return jsonify(data_)
+
+@app.route('/api/v1/search/', methods=['GET'])
+def search_anime():
+    url_ = base_url + "/filter?keyword=shingeki-no-kyojin"
+    data_ = getData(url_)
+    return jsonify(data_)
+
+app.run()
